f472747b380a72affb39cea8bbc2cb2595bc829f
Peje <guillepazerrobidart@yahoo.com.ar>
2021-11-22 14:48:51 -0300

    [MAKE CHANGES] on script.js file

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 
 /////// PROMESAS /////////
-
 /*new Promise((resolve, reject) => {
     if(true){
         resolve(true);
@@ -21,9 +20,6 @@
     .then(json => console.log(json))
     .catch(err => console.log('Error'));*/
 
-
-
-
     /// AXIOS ///
 /*
 const axios = require('axios');
@@ -39,36 +35,57 @@ axios.get('https://jsonplaceholder.typicode.com/todos/1')
         console.log('exec')
     }); */
 
+
+    //PRACTICAS//
     
 const axios = require('axios');
 
 /*const instance = axios.create({
     baseURL: 'https://jsonplaceholder.typicode.com'
 });
-
 instance.get('/todos/1')
 .then(res => {console.log(res.data)})
 .catch(err => console.log(err));
 */
 
-//CLIENTE HTTP PERSONALIZADO
-
+/*CLIENTE HTTP PERSONALIZADO
 const pokeHTTP = axios.create({
     baseURL: 'https://pokeapi.co/api/v2'    
 });
 
 pokeHTTP.get('/pokemon/1')
-.then(res => console.log('Pokemon: ', res.data.forms[0].name))
+.then(res => console.log('Pokemon: ', res.data.forms[0].name))*/
 
+/*const swapiHTTP =axios.create({
+    baseURL: 'https://swapi.dev/api/'
+});
 
+swapiHTTP.get('/people/1')
+.then(res => console.log('Star Wars: ', res.data.name))
+.catch(err => console.log(err))*/
 
+/*const climaHTTP = axios.create({
+    baseURL: 'https://climate-change-news.herokuapp.com/news'
+});
 
+climaHTTP.get('/thetimes')
+.then(res => console.log('Clima: ', res.data))
+.catch(err => console.log(err))*/
 
+/*const placeholderHTTP = axios.create({
+    baseURL: 'https://jsonplaceholder.typicode.com'
+});
 
+const searchId= {
+    params: {
+        id: 4
+    }
+};
 
-
-
-
+placeholderHTTP.get('/todos', searchId)
+.then(res => console.log('Placeholder: ', res.data))
+.catch(err => console.log(err))
+*/
 
 
 
